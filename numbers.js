cc6e47e2277787013fbc0510cd113f4e3c8eda33
Pabsyyy <christiangalinato1218@gmail.com>
2024-09-13 14:58:30 +0800

    initial commit

---
+++ numbers.js
@@ -0,0 +1,15 @@
+const firstFavNum = 12;
+const  secondFavNum = 18;
+
+const addition = firstFavNum + secondFavNum;
+const subtraction = firstFavNum - secondFavNum;
+const multiplication = firstFavNum * secondFavNum;
+const division = firstFavNum / secondFavNum;
+const modulus = firstFavNum % secondFavNum;
+const exponentiation = firstFavNum ** secondFavNum;
+console.log(addition);
+console.log(subtraction);
+console.log(multiplication);
+console.log(division);
+console.log(modulus);
+console.log(exponentiation);
